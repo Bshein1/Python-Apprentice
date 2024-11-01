b54f2cc9bede1c966c5616d13065c103b77fdee6
Blake Sheinberg <blakesheinberg@gmail.com>
2024-11-01 19:08:56 +0000

    Week 7 of Coding at SJA

--- lessons/00_Turtles/08b_More_Turtle_programs.py
+++ lessons/00_Turtles/08b_More_Turtle_programs.py
@@ -20,7 +20,23 @@ def set_turtle_image(turtle, image_name):
     turtle.shape(image_path)
 
 # Set up the screen
-screen = importturtle.Screen()
+import turtle
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+
+# Create a turtle and set its shape to the custom GIF
+t = turtle.Turtle()
+
+set_turtle_image(t, "pikachu.gif")
+
+t.penup()
+t.speed(3)
+
+for i in range(4):
+    t.goto(200, 200)
+    t.goto(-200, -200)
+# Set up the screen
+screen = turtle.Screen()
 screen.setup(width=600, height=600)
 
 # Create a turtle and set its shape to the custom GIF
@@ -36,4 +52,6 @@ for i in range(4):
     t.goto(-200, -200)
 
 
-turtle.exitonclick() 
+turtle.exitonclick()     
+
+ 
